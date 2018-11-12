282387b702bb5151328c80ee3a87a0192b1011ef
Niklas Hedström <dat15nhe@student.lu.se>
2018-11-12 18:30:43 +0100

    börjat på goodburger

--- src/GoodBurger.java
+++ src/GoodBurger.java
@@ -6,4 +6,20 @@ import org.jacop.search.*;
 public class GoodBurger {
 
 
+    public static void main(String[] args) {
+
+        Store store = new Store();
+
+        IntVar beef = new IntVar(store, "Beef", 1 , 5 );
+        IntVar bun = new IntVar(store, "Bun", 1 , 5 );
+        IntVar cheese = new IntVar(store, "Cheese", 1 , 5 );
+        IntVar onions = new IntVar(store, "Onions", 1 , 5 );
+        IntVar pickles = new IntVar(store, "Pickles", 1 , 5 );
+        IntVar lettuce = new IntVar(store, "Lettuce", 1 , 5 );
+        IntVar ketchup  = new IntVar(store, "Ketchup", 1 , 5 );
+        IntVar tomato   = new IntVar(store, "Tomato", 1 , 5 );
+
+        IntVar[] digits = {beef, bun, cheese, onions, pickles, lettuce, ketchup, tomato};
+    }
+
 }
