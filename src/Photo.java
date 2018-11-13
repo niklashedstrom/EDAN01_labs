f486f8289910f4cad2e0ebec80cf71b1c1e626c6
Emil Wihlander <emil.wihlander@outlook.com>
2018-11-13 16:50:28 +0100

    final 4.1

--- src/Photo.java
+++ src/Photo.java
@@ -18,8 +18,8 @@ class Photo {
 
 
         int choice = 0;
-        if (args.length != 0) {
-            choice = Integer.parseInt(args[0]);
+        if (args.length >= 2) {
+            choice = Integer.parseInt(args[1]);
         }
         switch (choice) {
             case 0:
@@ -51,8 +51,19 @@ class Photo {
                 break;
         }
 
+        int choice2 = 1;
 
-        solve(n, n_prefs, prefs);
+        if (args.length != 0) {
+            choice2 = Integer.parseInt(args[0]);
+        }
+
+        if (choice2 == 1) {
+            solve(n, n_prefs, prefs);
+        } else if (choice2 == 2) {
+            solve2(n, n_prefs, prefs);
+        } else {
+            System.out.println("not an option");
+        }
     }
 
     private static void solve(int n, int n_prefs, int[][] prefs) {
@@ -73,17 +84,19 @@ class Photo {
             int pers0 = prefs[i - 1][0];
             int pers1 = prefs[i - 1][1];
 
-            IntVar rel = new IntVar(store, "r_" + i, 0, 1);
-
-            relations.add(rel);
-            store.impose(new Reified(new Distance(
+            IntVar dist = new IntVar(store, "d_" + i, 1, n - 1);
+            store.impose(new Distance(
                 persons.get(pers0 - 1),
                 persons.get(pers1 - 1),
-                new IntVar(store,1,1)),
-                rel));
+                dist));
+
+            IntVar rel = new IntVar(store, "r_" + i, 0, 1);
+            relations.add(rel);
+
+            store.impose(new Reified(new XeqC(dist, 1), rel));
         }
 
-        IntVar totalDist = new IntVar(store, "total", -relations.size()*(n-1), 0);
+        IntVar totalRel = new IntVar(store, "total", -relations.size(), 0);
 
         List<IntVar> allVars = new ArrayList<IntVar>();
         allVars.addAll(persons);
@@ -95,7 +108,7 @@ class Photo {
 
         store.impose(new LinearInt(relations.toArray(new IntVar[0]),
                 inverters,
-                "==",totalDist));
+                "==",totalRel));
 
         long T1, T2;
         T1 = System.currentTimeMillis();
@@ -107,7 +120,67 @@ class Photo {
 
         Search<IntVar> search = new DepthFirstSearch<>();
 
-        boolean result = search.labeling(store, select, totalDist);
+        boolean result = search.labeling(store, select, totalRel);
+
+        if (result)
+            store.print();
+
+        T2 = System.currentTimeMillis();
+
+        System.out.println("\n\t*** Execution time = " + (T2 - T1) + " ms");
+    }
+
+    private static void solve2(int n, int n_prefs, int[][] prefs) {
+
+        Store store = new Store();
+
+        List<IntVar> persons = new ArrayList<IntVar>();
+
+        for (int i = 1; i <= n; i++) {
+            persons.add(new IntVar(store, "p_" + i, 1, n));
+        }
+
+        store.impose(new Alldiff(persons.toArray(new IntVar[0])));
+
+        List<IntVar> distances = new ArrayList<IntVar>();
+
+        for (int i = 1; i <= n_prefs; i++) {
+            int pers0 = prefs[i - 1][0];
+            int pers1 = prefs[i - 1][1];
+
+            IntVar dist = new IntVar(store, "d_" + i, 1, n - 1);
+            distances.add(dist);
+
+            store.impose(new Distance(
+                persons.get(pers0 - 1),
+                persons.get(pers1 - 1),
+                dist));
+        }
+
+        IntVar maxDist = new IntVar(store, "maxDist", 0, n - 1);
+
+        List<IntVar> allVars = new ArrayList<IntVar>();
+        allVars.addAll(persons);
+        allVars.addAll(distances);
+
+        int[] inverters = new int[n_prefs];
+        Arrays.fill(inverters, -1);
+
+
+        store.impose(new Max(distances.toArray(new IntVar[0]),
+                maxDist));
+
+        long T1, T2;
+        T1 = System.currentTimeMillis();
+
+        SelectChoicePoint<IntVar> select = new SimpleSelect<>(
+            allVars.toArray(new IntVar[0]),
+            null,
+            new IndomainMin<>());
+
+        Search<IntVar> search = new DepthFirstSearch<>();
+
+        boolean result = search.labeling(store, select, maxDist);
 
         if (result)
             store.print();
